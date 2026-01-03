8a54c4ed3d4c1ff0f93610f813a2602a99d63d83
inkwill8 <inkwill8@proton.me>
2026-01-03 17:18:37 -0500

    Type enums for Genre, Status and Rating

---
+++ include/types.h
@@ -0,0 +1,28 @@
+#pragma once
+
+enum class Genre {
+	BIOGRAPHY,
+	MEMOIR,
+	FANTASY,
+	SCIFI,
+	YOUNG_ADULT,
+	HISTORICAL_FICTION,
+	ROMANCE,
+	HORROR,
+	THRILLER
+};
+
+enum class Status {
+	WANT_TO_READ,
+	CURRENTLY_READING,
+	FINISHED,
+	DID_NOT_FINISH
+};
+
+enum class Rating {
+	1,
+	2,
+	3,
+	4,
+	5
+};
