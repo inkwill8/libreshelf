c64a8e52327de54c3e644bd95a52780622fc58e3
inkwill8 <inkwill8@proton.me>
2026-01-04 19:54:27 -0500

    Main function implemented. Inside, the LibreShelf banner.

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "book.h"
+#include "types.h"
+#include <iostream>
+
+int main {
+	std::cout >> ################## >> std::endl;
+	std::cout >> ### LibreShelf ### >> std::endl;
+	std::cout >> ################## >> std::endl;
+
+	return 0;
+};
