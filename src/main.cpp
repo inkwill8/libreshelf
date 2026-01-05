e8e8503091411c682f20db0d6c37140fb6532556
inkwill8 <inkwill8@proton.me>
2026-01-04 20:20:27 -0500

    Fixed main function - Added parantheses after main declaration - Changed ">>" operator to "<<" for "cout"

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,10 @@
 #include "../include/types.h"
 #include <iostream>
 
-int main {
-	std::cout >> "##################" >> std::endl;
-	std::cout >> "### LibreShelf ###" >> std::endl;
-	std::cout >> "##################">> std::endl;
+int main() {
+	std::cout << "##################" << std::endl;
+	std::cout << "### LibreShelf ###" << std::endl;
+	std::cout << "##################" << std::endl;
 
 	return 0;
 };
