f5584488ff8d6425ef4c1da3eadb5c265d60d33c
inkwill8 <inkwill8@proton.me>
2026-01-14 15:03:05 -0500

    Removed display menu from main and put into menu class.

---
+++ include/menu.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+
+class Menu {
+private:
+
+public:
+	void DisplayMainMenu();
+	void ShowAllBooks();
+	void SearchBooks();
+	void EditMetadata();
+	void RemoveBook();
+	void QuitMenu();
+
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include "../include/book.h"
 #include "../include/types.h"
-#include "../include/menu.h
+#include "../include/menu.h"
 #include <iostream>
 
 
@@ -9,20 +9,7 @@ int main() {
 	std::cout << "### LibreShelf ###" << std::endl;
 	std::cout << "##################" << std::endl;
 
-	std::cout << "Menu Choices:" << std::endl;
-	std::cout << "1. List All Books\n";
-	std::cout << "2. List Books by Status\n";
-	std::cout << "3. Search for Book\n";
-	std::cout << "4. Add Book\n";
-	std::cout << "5. Edit Metadata\n";
-	std::cout << "6. Remove Book\n";
-	std::cout << "0. Quit LibreShelf\n";
-	
-	// Initialize a varible for the menu choice and open an input
-	// stream from the console
-	int userChoice;
-	std::cin >> userChoice;
-
+	Menu::DisplayMainMenu();
 
 	return 0;
 };
