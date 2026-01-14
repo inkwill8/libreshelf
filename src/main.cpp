00ddfd4ca09d264d0af6c26cb89033dec4c50fc2
inkwill8 <inkwill8@proton.me>
2026-01-14 14:08:34 -0500

    Menu Options Added main menu options. Basic functionality of listing books, adding, removing, etc. Sub-menus have not been done yet.

--- src/main.cpp
+++ src/main.cpp
@@ -7,5 +7,14 @@ int main() {
 	std::cout << "### LibreShelf ###" << std::endl;
 	std::cout << "##################" << std::endl;
 
+	std::cout << "Menu Choices:" << std::endl;
+	std::cout << "1. List All Books\n";
+	std::cout << "2. List Books by Status\n";
+	std::cout << "3. Search for Book\n";
+	std::cout << "4. Add Book\n";
+	std::cout << "5. Add Metadata\n";
+	std::cout << "6. Remove Book\n";
+
+
 	return 0;
 };
