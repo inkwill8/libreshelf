095fb584c3e4ce404942e5a1d07c8331842fb571
inkwill8 <inkwill8@proton.me>
2026-01-14 14:46:59 -0500

    Menu Class Added menu class for better modularity. Class will include functions to display different sets of data and also a way to return to the main menu when desired.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include "../include/book.h"
 #include "../include/types.h"
+#include "../include/menu.h
 #include <iostream>
+
 
 int main() {
 	std::cout << "##################" << std::endl;
@@ -12,8 +14,14 @@ int main() {
 	std::cout << "2. List Books by Status\n";
 	std::cout << "3. Search for Book\n";
 	std::cout << "4. Add Book\n";
-	std::cout << "5. Add Metadata\n";
+	std::cout << "5. Edit Metadata\n";
 	std::cout << "6. Remove Book\n";
+	std::cout << "0. Quit LibreShelf\n";
+	
+	// Initialize a varible for the menu choice and open an input
+	// stream from the console
+	int userChoice;
+	std::cin >> userChoice;
 
 
 	return 0;
