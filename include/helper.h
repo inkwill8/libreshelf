514e00f29c7b6f4c0750d0d27dae9dda742eb36d
inkwill8 <inkwill8@proton.me>
2026-01-21 13:35:12 -0500

    Added helper implementatin file to separate function declarations and definitions

--- include/helper.h
+++ include/helper.h
@@ -3,42 +3,9 @@
 #include "types.h"
 
 namespace Helper {
-Genre StrToGenre(std::string genre) {
-	if (genre == "Biography") return Genre::BIOGRAPHY;
-	if (genre == "Memoir") return Genre::MEMOIR;
-	if (genre == "Fantasy") return Genre::FANTASY;
-	if (genre == "Sci-fi") return Genre::SCIFI;
-	if (genre == "Young Adult") return Genre::YOUNG_ADULT;
-	if (genre == "Historical Fiction") return Genre::HISTORICAL_FICTION;
-	if (genre == "Romance") return Genre::ROMANCE;
-	if (genre == "Horror") return Genre::HORROR;
-	if (genre == "Thriller") return Genre::THRILLER;
-};
-
-Status StrToStatus(std::string status) {
-	if (status == "want to read") return Status::WANT_TO_READ;
-	if (status == "currently reading") return Status::CURRENTLY_READING;
-	if (status == "finished") return Status::FINISHED;
-	if (status == "did not finish") return Status::DID_NOT_FINISH;
-};
-
-std::string GenreToStr(Genre genre) {
-	if (genre == Genre::BIOGRAPHY) return "Biography";
-	if (genre == Genre::MEMOIR) return "Memoir";
-	if (genre == Genre::FANTASY) return "Fantasy";
-	if (genre == Genre::SCIFI) return "Sci-fi";
-	if (genre == Genre::YOUNG_ADULT) return "Young Adult";
-	if (genre == Genre::HISTORICAL_FICTION) return "Historical Fiction";
-	if (genre == Genre::ROMANCE) return "Romance";
-	if (genre == Genre::HORROR) return "Horror";
-	if (genre == Genre::THRILLER) return "Thriller";
-};
-
-std::string StatusToStr(Status status) {
-	if (status == Status::WANT_TO_READ) return "want to read";
-	if (status == Status::CURRENTLY_READING) return "currently reading";
-	if (status == Status::FINISHED) return "finished";
-	if (status == Status::DID_NOT_FINISH) return "did not finish";
-};
+	Genre StrToGenre(std::string genre);
+	Status StrToStatus(std::string status);
+	std::string GenreToStr(Genre genre);
+	std::string StatusToStr(Status status);
 }
 
