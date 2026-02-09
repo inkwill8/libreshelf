29d4fd032ffd893d853ec9d6dddd3ad0e67fe4c8
inkwill8 <inkwill8@proton.me>
2026-02-08 21:16:36 -0500

    Fix bugs in ToStandardFormat - Changes assignment to comparison in (i = 0) - Changes string literal to character literal - Uses previous letter instead of next to fix out of bounds issue - Declares prevLetter outside for loop - Ensures first letter is only added to return string once

--- src/helper.cpp
+++ src/helper.cpp
@@ -63,25 +63,25 @@ std::string ToLowercase(std::string word) {
 // Makes a string where the first letter of each word is capitalized
 std::string ToStandardFormat(std::string word) {
 	std::string formattedWord = "";
+	char prevLetter;
 
 	for (int i = 0; i < word.size(); i++) {
 	  char letter = word[i];
-	  char nextLetter = word[i + 1];
+	  prevLetter = word[i - 1];
 
 	  // Make sure the first letter is always capitalized
-	  if (i = 0) {
+	  if (i == 0) {
 		letter = static_cast<char>(std::toupper(letter));
 		formattedWord.push_back(letter);
 	  }
 
 	  // If the letter is a space, i.e. it's a new word, capitalize the next letter
-	  if (letter == " ") {
-		formattedWord.push_back(letter);
-		nextLetter = static_cast<char>(std::toupper(nextLetter));
+	  if (prevletter == ' ') {
+		letter = static_cast<char>(std::toupper(letter));
 	  }
 
 	  // To ensure all letters still get added to return string
-	  formattedWord.push_back(letter);
+	  if (i != 0) { formattedWord.push_back(letter); };
 	}
 
 	return formattedWord;
