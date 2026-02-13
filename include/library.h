34bb5404dd5ba180b8e2eb0d01367c99a0119d7d
inkwill8 <inkwill8@proton.me>
2026-02-13 15:49:51 -0600

    Add filename private member field and private methods for CSV analysis

--- include/library.h
+++ include/library.h
@@ -1,23 +1,28 @@
 #pragma once
 #include "book.h"
-#include <vector>
 #include <fstream>
 #include <sstream>
+#include <vector>
 
 class Library {
 
 private:
-	std::vector<Book> library;
+  std::vector<Book> library;
+  std::string filename = "../data/books.csv";
+
+  // CSV METHODS
   std::vector<Book> ReadFromCSV(const std::string &filename);
+  Book RemoveFromCSV(const std::string &filename,
+                     const std::string &bookToDelete);
 
 public:
-	void LoadLibrary(const std::string& filename);
-	bool AppendToCSV(const std::string& filename, Book book);
-	std::vector<Book*> SearchBooks(const std::string& bookName);
-	bool AddBook(std::string& title, std::string& author, std::string& isbn, std::string& genre, std::string& status, std::string& rating);
-  bool RemoveBook(const std::string& bookToDelete);
-	void EditMetadata();
+  void LoadLibrary(const std::string &filename);
+  bool AppendToCSV(const std::string &filename, Book book);
+  std::vector<Book *> SearchBooks(const std::string &bookName);
+  bool AddBook(std::string &title, std::string &author, std::string &isbn,
+               std::string &genre, std::string &status, std::string &rating);
+  bool RemoveBook(const std::string &bookToDelete);
+  void EditMetadata();
 
-
-	std::vector<Book> GetAllBooks() const;
+  std::vector<Book> GetAllBooks() const;
 };
