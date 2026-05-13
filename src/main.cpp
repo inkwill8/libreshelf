c79ac502932cdcd9dd5ab6054849ac710fd0ca8c
inkwill8 <inkwill8@proton.me>
2026-05-13 21:34:48 +0700

    feat(main): initialize curl global env

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,26 @@
 #include "../include/menu.h"
+#include <curl/curl.h>
+#include <curl/urlapi.h>
 #include <iostream>
 
 int main() {
-  std::cout << "##################" << std::endl;
-  std::cout << "### LibreShelf ###" << std::endl;
-  std::cout << "##################" << std::endl;
 
-  Menu mainMenu;
-  mainMenu.DisplayMainMenu();
+  CURLcode result = curl_global_init(CURL_GLOBAL_ALL);
+
+  if (result == CURLE_OK) {
+
+    std::cout << "##################" << std::endl;
+    std::cout << "### LibreShelf ###" << std::endl;
+    std::cout << "##################" << std::endl;
+
+    Menu mainMenu;
+    mainMenu.DisplayMainMenu();
+
+    curl_global_cleanup();
+
+  } else {
+    std::cout << curl_easy_strerror(result);
+  }
 
   return 0;
 };
