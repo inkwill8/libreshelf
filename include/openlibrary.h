5ec50667c9fd57f044b6a804d8db7a37169952fe
inkwill8 <inkwill8@proton.me>
2026-05-14 20:08:57 +0700

    feat(openlibclient): define class instance methods

--- include/openlibrary.h
+++ include/openlibrary.h
@@ -1,3 +1,20 @@
 #pragma once
+#include "book.h"
+#include "json.hpp"
 #include <curl/curl.h>
+#include <string>
 
+class OpenLibClient {
+
+private:
+  // Helper method to perform the curl get request and return the raw string
+  std::string PerformGetRequest(const std::string &url);
+
+public:
+  std::vector<Book> SearchByTitle(const std::string &title);
+  std::vector<Book> SearchByIsbn(const std::string &isbn);
+  std::vector<Book> SearchByAuthor(const std::string &isbn);
+
+  // In case we want to include series search functionality and parsing of
+  // OpenLibrary edition info
+};
