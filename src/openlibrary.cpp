52da5c528b6e54209202729923466588ea934873
inkwill8 <inkwill8@proton.me>
2026-05-14 21:51:57 +0700

    build(openlibclient): UrlEncode method
    Input urls with spaces and special characters like apostrophes should
    not crash the client now.

--- src/openlibrary.cpp
+++ src/openlibrary.cpp
@@ -18,6 +18,7 @@ std::string OpenLibClient::PerformGetRequest(const std::string &url) {
   if (curl) {
     curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallBack);
+    curl_easy_setopt(curl, CURLOPT_TIMEOUT, 10L);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, &responseBuffer);
 
     CURLcode result = curl_easy_perform(curl);
@@ -32,4 +33,15 @@ std::string OpenLibClient::PerformGetRequest(const std::string &url) {
   }
 
   return responseBuffer;
+};
+
+std::string OpenLibClient::UrlEncode(const std::string &input) {
+  CURL *curl = curl_easy_init();
+
+  char *encoded = curl_easy_escape(curl, input.c_str(), input.length());
+  std::string result = encoded;
+  curl_free(encoded);
+  curl_easy_cleanup(curl);
+
+  return result;
 };
