450fb1c9a0cfe75d31c539e108d333e205d8ac0c
inkwill8 <inkwill8@proton.me>
2026-05-14 21:08:55 +0700

    build(openlibclient): add callback function
    Adds the static member function WriteCallBack to be used by the
    PerformGetRequest method for api calls to OpenLibrary

--- include/openlibrary.h
+++ include/openlibrary.h
@@ -7,14 +7,18 @@
 class OpenLibClient {
 
 private:
-  // Helper method to perform the curl get request and return the raw string
+  // Helper methods to perform the curl get request and return the raw string
+  static size_t WriteCallBack(void *contents, size_t size, size_t nmemb,
+                              std::string *output);
   std::string PerformGetRequest(const std::string &url);
+  std::string UrlEncode(const std::string &input);
 
 public:
-  std::vector<Book> SearchByTitle(const std::string &title);
-  std::vector<Book> SearchByIsbn(const std::string &isbn);
-  std::vector<Book> SearchByAuthor(const std::string &isbn);
+  std::optional<std::vector<Book>> SearchByTitle(const std::string &title);
+  std::optional<std::vector<Book>> SearchByIsbn(const std::string &isbn);
+  std::optional<std::vector<Book>> SearchByAuthor(const std::string &isbn);
 
   // In case we want to include series search functionality and parsing of
   // OpenLibrary edition info
+  std::vector<Book> SearchBySeries(const std::string &series);
 };
